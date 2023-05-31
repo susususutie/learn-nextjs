9c2667f1dfa8a0f26ef79de20856055eeb31afe6
sutie <sususu_sutie@qq.com>
2023-05-31 20:14:34 +0800

    feat: 1.create page

---
+++ pages/posts/first-post.js
@@ -0,0 +1,10 @@
+export default function FirstPost() {
+  return (
+    <>
+      <h1>First Post</h1>
+      <h1 className="title">
+        Welcome to <a href="https://nextjs.org">Next.js!</a>
+      </h1>
+    </>
+  );
+}
