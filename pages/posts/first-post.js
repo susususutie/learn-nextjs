c7653b175b715b80b142dda84477af1c8c8f58ad
sutie <sususu_sutie@qq.com>
2023-05-31 20:15:30 +0800

    feat: 2. navigate between pages with Link

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,9 +1,14 @@
+import Link from "next/link";
+
 export default function FirstPost() {
   return (
     <>
       <h1>First Post</h1>
       <h1 className="title">
         Welcome to <a href="https://nextjs.org">Next.js!</a>
+      </h1>
+      <h1 className="title">
+        Read <Link href="/">Home</Link>
       </h1>
     </>
   );
