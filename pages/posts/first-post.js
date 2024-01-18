dcf62ff5cf9634710c0b057e6ea4e83e3258455b
sutie <sususu_sutie@qq.com>
2024-01-18 17:41:30 +0800

    3.4 CSS

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,26 +1,17 @@
 import Head from "next/head";
 import Link from "next/link";
-import Script from 'next/script';
+import Layout from "../../components/layout";
+
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
       <Head>
         <title>First Post</title>
       </Head>
-      <Script
-        src="https://connect.facebook.net/en_US/sdk.js"
-        strategy="lazyOnload"
-        onLoad={() =>
-          console.log(`script loaded correctly, window.FB has been populated`)
-        }
-      />
       <h1>First Post</h1>
-      <h1 className="title">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
-      </h1>
-      <h1 className="title">
-        Read <Link href="/">Home</Link>
-      </h1>
-    </>
+      <h2>
+        <Link href="/">← Back to home</Link>
+      </h2>
+    </Layout>
   );
 }
